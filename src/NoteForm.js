d8422f41d2fc05ed13c264fea5c2a798987f12cd
dimpledhawan <dhawan.dimple21@gmail.com>
2018-05-22 11:35:11 -0400

    styling

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const NoteForm = () => {
-    return (
-        <div className="NoteForm"> 
-            NoteForm 
-        </div>
-    )
+  return (
+    <div className="NoteForm">
+      NoteForm
+    </div>
+  )
 }
 
 export default NoteForm
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Sidebar = () => {
-    return (
-        <div className="Sidebar"> 
-            Sidebar 
-        </div>
-    )
+  return (
+    <div className="Sidebar">
+      Sidebar
+    </div>
+  )
 }
 
 export default Sidebar
